750f3df16ff2af40ca51cd0564ddcb1241c334db
ghr85 <ghrenfrew@gmail.com>
2019-01-22 17:44:08 +0000

    added fish count functionality to River class

--- multi_classes_hw_river.rb
+++ multi_classes_hw_river.rb
@@ -2,10 +2,19 @@
 # Multiple Class Interaction and Class Diagrams
 # Ecosystem Homework - Fish Class
 
+#fish array, add fish, count fish
 class River
+
 attr_reader :name_string
-  def initialize(name_string)
+attr_accessor :fish_objects_array
+
+  def initialize(name_string,*fish_object_args) #SPLAT! One argument, creates an array for you.
     @name_string = name_string
+    @fish_objects_array = fish_object_args #Note that splat doesn't exist in Java - elipsis (...) does the same trick
+  end
+
+  def count_fish
+    return @fish_objects_array.count
   end
 
 end
--- specs/multi_classes_hw_river_spec.rb
+++ specs/multi_classes_hw_river_spec.rb
@@ -9,14 +9,21 @@ require_relative ('../multi_classes_hw_fish.rb')
 
 class RiverTest < MiniTest::Test #inherit methods from minitest package
 
-  def setup #set up initial data
-    @river_name_string = River.new("Euphrates")
+  def setup #set up initial data, note we're going to use a splat operator for ease
+
     @fish_object_1 = Fish.new("Wonda")
     @fish_object_2 = Fish.new("Nemo")
     @fish_object_3 = Fish.new("Jaws")
+    @river_object = River.new("Euphrates",@fish_object_1,@fish_object_2, @fish_object_3)
   end
 
-  def test_get_river_name
-    assert_equal("Euphrates", @river_name_string.name_string)
+  def test_get_river_name #test get fish method using attr_reader
+    assert_equal("Euphrates", @river_object.name_string)
+  end
+
+  def test_count_fish # test ability to count fish in array
+
+    assert_equal(3,@river_object.count_fish)
+    
   end
 end
