263978424b0dd6518c74c5cbf6bc3fe574bc8fd4
ghr85 <ghrenfrew@gmail.com>
2019-01-22 17:06:46 +0000

    added get fish name functionality

--- specs/multi_classes_hw_bear_spec.rb
+++ specs/multi_classes_hw_bear_spec.rb
@@ -1,3 +1,7 @@
 # Codeclan Week 02 Day 02
 # Multiple Class Interaction and Class Diagrams
 # Ecosystem Homework - Bear Test Specifications
+
+require ('minitest/autorun')
+require ('minitest/rg')
+require_relative ('../bear.rb')
--- specs/multi_classes_hw_fish_spec.rb
+++ specs/multi_classes_hw_fish_spec.rb
@@ -1,3 +1,17 @@
 # Codeclan Week 02 Day 02
 # Multiple Class Interaction and Class Diagrams
 # Ecosystem Homework - Fish Test Specifications
+
+require ('minitest/autorun')
+require ('minitest/rg')
+require_relative ('../multi_classes_hw_fish.rb')
+
+class FishTest < MiniTest::Test #inherit methods from minitest package
+  def setup #create a fish object for test purposes, supply with mildly amusing name
+    @fish = Fish.new("Wonda") # <- INstance variable for THIS instance of fish
+  end
+
+  def test_get_fish_name #test fish name assignment worked
+    assert_equal("Wonda", @fish.name_string) #using an accessor method rather than a manual getter.
+  end
+end
