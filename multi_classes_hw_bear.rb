0f97f178aeb9387ab33a69dfa312eda5eda0d874
ghr85 <ghrenfrew@gmail.com>
2019-01-22 18:55:30 +0000

    Added comments for later transparency

--- multi_classes_hw_bear.rb
+++ multi_classes_hw_bear.rb
@@ -5,13 +5,19 @@
 class Bear
   attr_reader :name_string, :type_string
   attr_accessor :food_array
- def initialize(name_string,type_string)
-   @name_string = name_string
-   @type_string = type_string
-   @food_array = []
- end
 
- def get_fish(river_object)
-   @food_array << river_object.fish_objects_array.pop
- end
+  def initialize(name_string,type_string) # set up inital data
+    @name_string = name_string
+    @type_string = type_string
+    @food_array = [] #this will always be an empty array, assume it's post hibernation
+  end
+
+  def get_fish(river_object)
+    @food_array << river_object.fish_objects_array.pop #LIFO
+    return @food_array
+  end
+
+  def count_food
+    return @food_array.count #count elements in food array
+  end
 end
--- specs/multi_classes_hw_bear_spec.rb
+++ specs/multi_classes_hw_bear_spec.rb
@@ -11,23 +11,32 @@ require_relative ('../multi_classes_hw_bear.rb')
 class BearTest < MiniTest::Test #inherit methods from minitest package
 
   def setup #set up initial data, note we're going to use a splat operator for ease
-    @fish_object_1 = Fish.new("Wonda")
+    @fish_object_1 = Fish.new("Wonda") #create fish for river - could arguably do this in river object but this feels cleaner?
     @fish_object_2 = Fish.new("Nemo")
     @fish_object_3 = Fish.new("Jaws")
-    @river_object = River.new("Tigris",@fish_object_1, @fish_object_2, @fish_object_3)
+    @river_object = River.new("Tigris",@fish_object_1, @fish_object_2, @fish_object_3) 
     @bear_object = Bear.new("Paddington","Deepest Darkest Peruvian")
-end
-  def test_get_name
+  end
+  def test_get_name # check that you can access bear name
     assert_equal("Paddington",@bear_object.name_string)
   end
 
-  def test_get_fish__bear
-    @bear_object.get_fish(@river_object)
-    assert_equal([@fish_object_3],@bear_object.food_array)
+  def test_get_type #check that you can access bear type
+    assert_equal("Deepest Darkest Peruvian",@bear_object.type_string)
   end
-  def test_get_fish__river
+
+  def test_get_fish__bear #Check that get fish function moves last fish from river to bear
+    @bear_object.get_fish(@river_object)
+    assert_equal([@fish_object_3],@bear_object.food_array) #remember brackets around fish object
+  end
+  def test_get_fish__river # Check that same function removes fish from river
     @bear_object.get_fish(@river_object)
     assert_equal(2,@river_object.count_fish)
   end
 
+  def test_count_food #count food items, check for multiples
+    @bear_object.get_fish(@river_object)
+    @bear_object.get_fish(@river_object)
+    assert_equal(2, @bear_object.count_food)
+  end
 end
